dbd8e69cc3c7106391552648fdd33a707c8a36aa
varun05126 <malthumkarvarun@gmail.com>
2026-06-17 23:02:39 +0530

    backend fix

---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/career/', include('career.urls')),
+    path('api/learning/', include('learning.urls')),
+    path('api/resources/', include('resources.urls')),
+    path('api/analytics/', include('analytics.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
